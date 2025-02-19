e2667d651c1efafbc8e9693a33fd86d1258aabc2
Favas abdul nassar <favasabdulnassar@gmail.com>
2025-02-20 03:16:52 +0530

    Deployed api added

--- src/services/intercepor.js
+++ src/services/intercepor.js
@@ -6,7 +6,7 @@ import { logout } from "../Redux/loginReducer";
 
 
 const axiosInstance = axios.create({
-    baseURL:"http://127.0.0.1:8000",
+    baseURL:"https://appsite-backend.onrender.com",
 });
 
 
